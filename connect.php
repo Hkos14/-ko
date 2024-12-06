441e18a0105c95d8db16eaedabfacdd81b549c46
Marcalek Máté <matemarcalek@gmail.com>
2024-12-06 12:49:35 +0100

    VMi

--- connect.php
+++ connect.php
@@ -1,14 +1,16 @@
 <?php
 $server = "localhost";
-$username = "username";
-$password = "";
+$username = "englishoko";
+$password = "Akos123456";
 $database = "englishoko";
 
 // Create connection
-$dbconn = new mysqli($server, $username, $password, $database);
+$dbconn = new mysqli($server, $username, $password, $database, 3306);
 
 // Check connection
 if ($dbconn->connect_error) {
+  echo "Kapcsolati hiba";
   die("Connection failed: " . $dbconn->connect_error);
 }
+else echo "Kapcsolat";
 ?>
